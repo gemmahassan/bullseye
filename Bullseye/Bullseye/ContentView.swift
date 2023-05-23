416de4800196f8a28bba5be9e55f06a37e6b92c1
Gemma <gemmaehassan@gmail.com>
2023-05-23 22:00:28 +0100

    challenge - add view modifiers

--- Bullseye/Bullseye/ContentView.swift
+++ Bullseye/Bullseye/ContentView.swift
@@ -17,11 +17,16 @@ struct ContentView: View {
         .font(.footnote)
         .kerning(2.0)
       Text("89")
+        .fontWeight(.black)
+        .font(.largeTitle)
+        .kerning(-1.0)
       HStack {
         Text("1")
+          .bold()
         Slider(value: .constant(50),
                in: 1.0...100.0)
         Text("100")
+          .bold()
       }
       Button("Hit me") {
       }
