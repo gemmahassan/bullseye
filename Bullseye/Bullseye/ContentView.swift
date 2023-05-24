38e975ae30b08b4fd38a2fcb97b0ac590af80260
Gemma <gemmaehassan@gmail.com>
2023-05-24 21:22:39 +0100

    makes alert text dynamic

--- Bullseye/Bullseye/ContentView.swift
+++ Bullseye/Bullseye/ContentView.swift
@@ -49,7 +49,8 @@ struct ContentView: View {
           }
         },
         message: {
-          Text("This is my first alert")
+          var roundedValue: Int = Int(sliderValue.rounded())
+          Text("The slider's value is \(roundedValue).")
         }
       )
     }
